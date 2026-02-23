2fbc0e6563af9bf5e959671d4fd5cb951167224f
thien20022009 <sttnguyenchithien512@gmail.com>
2026-02-23 10:32:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 async function askGemini(question) {
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + process.env.GEMINI_API_KEY,
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
